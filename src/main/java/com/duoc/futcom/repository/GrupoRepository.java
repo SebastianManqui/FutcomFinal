32200d781afb8fc191419d1cc1e0d7bbd0faaa61
SebastianManqui <se.manqui@duocuc.cl>
2026-04-24 14:01:11 -0400

    Se crearon los demas objetos en el repository con el extends Jpa

---
+++ src/main/java/com/duoc/futcom/repository/GrupoRepository.java
@@ -0,0 +1,8 @@
+package com.duoc.futcom.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository; 
+import com.duoc.futcom.model.Jugador;
+
+public interface GrupoRepository extends JpaRepository<Jugador, Integer> {
+    
+}
