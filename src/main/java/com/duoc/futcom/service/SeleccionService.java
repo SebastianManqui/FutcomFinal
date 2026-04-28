52d977912c325beaa567a724e5e5aab8aede6a28
sebastian <se.manqui@duocuc.cl>
2026-04-28 14:10:33 -0400

    + cambioss

---
+++ src/main/java/com/duoc/futcom/service/SeleccionService.java
@@ -0,0 +1,33 @@
+package com.duoc.futcom.service;
+import com.duoc.futcom.model.Seleccion;
+import com.duoc.futcom.repository.SeleccionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class SeleccionService {
+
+    @Autowired
+    private SeleccionRepository seleccionRepo;
+
+
+    public List<Seleccion> listarTodas() {
+        return seleccionRepo.findAll();
+    }
+
+    
+    public void guardaSeleccion(Seleccion seleccion) {
+        seleccionRepo.save(seleccion);
+    }
+
+    
+    public Seleccion buscarSeleccionPorId(int id) {
+        return seleccionRepo.findById(id).orElse(null);
+    }
+
+
+    public void eliminarSeleccion(int id) {
+        seleccionRepo.deleteById(id);
+    }
+}
