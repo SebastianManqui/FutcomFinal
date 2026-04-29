da1a8c35d73bc29e9e696854a63c3a8a16cef14b
SebastianManqui <se.manqui@duocuc.cl>
2026-04-29 12:13:59 -0400

    + cambios

---
+++ src/main/java/com/duoc/futcom/controller/JugadorController.java
@@ -0,0 +1,13 @@
+package com.duoc.futcom.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.duoc.futcom.model.Jugador;
+import com.duoc.futcom.service.JugadorService;
+
+@RestController
+@RequestMapping("/api/v1/jugadores")
+public class JugadorController {
+
+}
