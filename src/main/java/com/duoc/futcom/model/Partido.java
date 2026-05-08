f58f39e30a34fb866996c5b13faf715af639e59f
SebastianManqui <se.manqui@duocuc.cl>
2026-05-08 13:28:00 -0400

    correcion de sintaxis

--- src/main/java/com/duoc/futcom/model/Partido.java
+++ src/main/java/com/duoc/futcom/model/Partido.java
@@ -1,26 +1,26 @@
 package com.duoc.futcom.model;
+import lombok.Data; 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
+import lombok.AllArgsConstructor;
 
-
+@Data 
 @Entity
+@Table(name = "partido")
 public class Partido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_partido")
     private int id_partido;
     
     @NotBlank
     private String nombre;
 
-     @ManyToOne
+    @ManyToOne
     @JoinColumn(name = "estadio_id") 
     private Estadio estadio;
-  
+    
 }
